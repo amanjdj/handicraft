bfc798bc67a954f6dba13df8099540f6194369a3
iamalok12 <ialok@outlook.com>
2021-05-21 14:39:03 +0530

    auth bug removal crop added bulk order added outof stock option added customer home implemented

--- lib/sellerhomewidgets.dart
+++ lib/sellerhomewidgets.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:handicraft/splashScreen.dart';
@@ -10,25 +11,85 @@ import 'package:handicraft/data/data.dart';
 import 'package:handicraft/login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import '../orderpages.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
-class OrdersArrived extends StatelessWidget {
-  const OrdersArrived({Key key}) : super(key: key);
+class OrdersArrived extends StatefulWidget {
+
+  @override
+  _OrdersArrivedState createState() => _OrdersArrivedState();
+}
+
+class _OrdersArrivedState extends State<OrdersArrived> {
+  List<CustomerPanel> list=[];
+
+  Future<void> fetchOrders()async{
+    list.clear();
+    var data=await FirebaseFirestore.instance.collection("Orders").where("seller",isEqualTo: App.sharedPreferences.getString("email")).get();
+    for(int i=0;i<data.docs.length;i++){
+      var img=await FirebaseFirestore.instance.collection("Items").doc(data.docs[i].data()['itemId']).get();
+      var imgUrl=img.data()['imageURL'];
+      CustomerPanel item=CustomerPanel(data.docs[i].data()['title'],imgUrl.toString(),data.docs[i].data()['pinCode']);
+      list.add(item);
+    }
+    setState(() {
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    fetchOrders();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-          color: Colors.transparent,
-          child: SingleChildScrollView(
-            child: SafeArea(
-              child: Text(App.sharedPreferences.getString("email")),
-            ),
-          )),
+      child: SafeArea(
+        child: RefreshIndicator(
+          onRefresh: fetchOrders,
+          child: Column(
+            children: [
+              Expanded(child:ListView.builder(itemCount: list.length,
+              itemBuilder: (_,index){
+                return list.length==0?Text("No Orders",style: TextStyle(color: Colors.white),):CustomerUI(list[index].title, list[index].imageurl, list[index].pincode);
+              },
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget CustomerUI(String title,String imageurl,String pincode){
+    return Container(
+      child: Column(
+        children: [
+          Text(title),
+          Container(
+            height: 200,
+            width: 200,
+            child:Image.network(imageurl) ,
+          ),
+          Text(pincode)
+        ],
+      ),
     );
   }
 }
+class CustomerPanel{
+  String title,imageurl,pincode;
+  CustomerPanel(this.title,this.imageurl,this.pincode);
+}
+
+
+
+
+
+
+
+
+
+
 
 class ItemModify extends StatefulWidget {
   const ItemModify({Key key}) : super(key: key);
@@ -36,7 +97,6 @@ class ItemModify extends StatefulWidget {
   @override
   _ItemModifyState createState() => _ItemModifyState();
 }
-
 class _ItemModifyState extends State<ItemModify> {
   @override
   void initState() {
