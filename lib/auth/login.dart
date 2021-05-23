616e77fd0cb443bd9431561fe0e3d584bbd652b8
amanjdj <amanjdj@outlook.com>
2021-05-23 22:44:46 +0530

    loginpage theme update

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -22,81 +22,119 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
   // SharedPreferences sharedPreferences;
   @override
   Widget build(BuildContext context) {
-    double width=MediaQuery.of(context).size.width;
-    double height=MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: AnimatedBackground(
-        vsync:this,
-        behaviour: BubblesBehaviour(),
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.center,
-         crossAxisAlignment: CrossAxisAlignment.center,
-         children: [
-           Container(
-             decoration: BoxDecoration(
-               image: DecorationImage(
-                 image: AssetImage("images/logo.png"),
-               )
-             ),
-             height: height*0.2,
-             width: height*0.2,
-           ),
-           SizedBox(height: 20,),
-           Center(
-             child: Container(
-               height: height*0.09,
-               width: width*0.6,
-               child: ElevatedButton(onPressed: (){
-                 void signIn()async{
-                   await _googleSignIn.signIn().then((value) async {
-                     print(_googleSignIn.currentUser.displayName);
-                     var data =await FirebaseFirestore.instance.collection("users").where("email",isEqualTo: _googleSignIn.currentUser.email).get();
-                     if(data.docs.isNotEmpty){
-                       data.docs.forEach((element) async {
-                         type=element.data()["type"];
-                         await App.sharedPreferences.setString("type", type);
-                         await App.sharedPreferences.setString("email", _googleSignIn.currentUser.email);
-                         await App.sharedPreferences.setString("url", _googleSignIn.currentUser.photoUrl);
-                         await App.sharedPreferences.setString("name", _googleSignIn.currentUser.displayName);
-                         if(type=="seller"){
-                           Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SellerHome()));
-                         }
-                         else if(type=="customer"){
-                           Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SidebarLayout()));
-                         }
-                       });
-                     }
-                     else if(data.docs.isEmpty){
-                       await App.sharedPreferences.setString("email", _googleSignIn.currentUser.email);
-                       await App.sharedPreferences.setString("url", _googleSignIn.currentUser.photoUrl);
-                       await App.sharedPreferences.setString("name", _googleSignIn.currentUser.displayName);
-                       await App.sharedPreferences.setString("type", "null");
-                       await Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Register(name: _googleSignIn.currentUser.displayName,imageUrl: _googleSignIn.currentUser.photoUrl,email: _googleSignIn.currentUser.email,)));
-                       print("empty");
-                     }
-                   });//google sign
-                   // final GoogleSignInAuthentication authentication =
-                   // await account.authentication;
-                   //
-                   // final GoogleAuthCredential credential = GoogleAuthProvider.credential(
-                   //     idToken: authentication.idToken,
-                   //     accessToken: authentication.accessToken);
-                   //
-                   // final UserCredential authResult =
-                   // await _auth.signInWithCredential(credential);
-                   // final User user = authResult.user;
-                   //
-                   // print(user);
+        backgroundColor: Colors.black12,
+        body: AnimatedBackground(
+          vsync: this,
+          behaviour: BubblesBehaviour(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage("images/logo.png"),
+                )),
+                height: height * 0.2,
+                width: height * 0.2,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Container(
+                  height: height * 0.09,
+                  width: width * 0.6,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        void signIn() async {
+                          await _googleSignIn.signIn().then((value) async {
+                            print(_googleSignIn.currentUser.displayName);
+                            var data = await FirebaseFirestore.instance
+                                .collection("users")
+                                .where("email",
+                                    isEqualTo: _googleSignIn.currentUser.email)
+                                .get();
+                            if (data.docs.isNotEmpty) {
+                              data.docs.forEach((element) async {
+                                type = element.data()["type"];
+                                await App.sharedPreferences
+                                    .setString("type", type);
+                                await App.sharedPreferences.setString(
+                                    "email", _googleSignIn.currentUser.email);
+                                await App.sharedPreferences.setString(
+                                    "url", _googleSignIn.currentUser.photoUrl);
+                                await App.sharedPreferences.setString("name",
+                                    _googleSignIn.currentUser.displayName);
+                                if (type == "seller") {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => SellerHome()));
+                                } else if (type == "customer") {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              SidebarLayout()));
+                                }
+                              });
+                            } else if (data.docs.isEmpty) {
+                              await App.sharedPreferences.setString(
+                                  "email", _googleSignIn.currentUser.email);
+                              await App.sharedPreferences.setString(
+                                  "url", _googleSignIn.currentUser.photoUrl);
+                              await App.sharedPreferences.setString("name",
+                                  _googleSignIn.currentUser.displayName);
+                              await App.sharedPreferences
+                                  .setString("type", "null");
+                              await Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Register(
+                                            name: _googleSignIn
+                                                .currentUser.displayName,
+                                            imageUrl: _googleSignIn
+                                                .currentUser.photoUrl,
+                                            email:
+                                                _googleSignIn.currentUser.email,
+                                          )));
+                              print("empty");
+                            }
+                          }); //google sign
+                          // final GoogleSignInAuthentication authentication =
+                          // await account.authentication;
+                          //
+                          // final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+                          //     idToken: authentication.idToken,
+                          //     accessToken: authentication.accessToken);
+                          //
+                          // final UserCredential authResult =
+                          // await _auth.signInWithCredential(credential);
+                          // final User user = authResult.user;
+                          //
+                          // print(user);
+                        }
 
-                 }
-                 signIn();
-               }, child: Text("Sign in using Google",style: TextStyle(fontSize: 20),)),
-             ),
-           ),
-
-         ],
-       ),
-      )
-    );
+                        signIn();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xff282C31),
+                        elevation: 20,
+                      ),
+                      child: Text(
+                        "Sign in using Google",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
